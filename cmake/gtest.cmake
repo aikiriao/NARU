81fbd323cf19bcc51887ef824843eb406802f52f
aikiriao <samuraiaiki@gmail.com>
2021-04-01 14:56:01 +0900

    update gtest version: 1.7.0 -> 1.10.0

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -6,14 +6,14 @@ include(ExternalProject)
 # gtest追加
 ExternalProject_Add(GoogleTest
     GIT_REPOSITORY  https://github.com/google/googletest.git
-    GIT_TAG         release-1.7.0
+    GIT_TAG         release-1.10.0
     INSTALL_COMMAND   ""
     LOG_DOWNLOAD ON
     )
 
 # インクルードパス追加
 ExternalProject_Get_Property(GoogleTest source_dir)
-include_directories(${source_dir}/include)
+include_directories(${source_dir}/googletest/include)
 
 # 成果物のディレクトリ取得
 ExternalProject_Get_Property(GoogleTest binary_dir)
@@ -22,12 +22,12 @@ ExternalProject_Get_Property(GoogleTest binary_dir)
 add_library(gtest STATIC IMPORTED)
 set_property(
     TARGET gtest
-    PROPERTY IMPORTED_LOCATION ${binary_dir}/libgtest.a
+    PROPERTY IMPORTED_LOCATION ${binary_dir}/lib/libgtest.a
     )
 
 # メインエントリ追加
 add_library(gtest_main STATIC IMPORTED)
 set_property(
     TARGET gtest_main
-    PROPERTY IMPORTED_LOCATION ${binary_dir}/libgtest_main.a
+    PROPERTY IMPORTED_LOCATION ${binary_dir}/lib/libgtest_main.a
     )
