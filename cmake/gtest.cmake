847f34a66e59ec6231c9a26c5a8266f456d648e8
aikiriao <samuraiaiki@gmail.com>
2021-04-02 19:35:39 +0900

    add release build library in windows test.

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -21,9 +21,14 @@ ExternalProject_Get_Property(GoogleTest binary_dir)
 # ライブラリ追加
 add_library(gtest STATIC IMPORTED)
 if(MSVC)
+    if(CMAKE_BUILD_TYPE STREQUAL "Release")
+        set(libgtest_path "${binary_dir}/lib/Release/gtest.lib")
+    else()
+        set(libgtest_path "${binary_dir}/lib/Debug/gtestd.lib")
+    endif()
     set_property(
         TARGET gtest
-        PROPERTY IMPORTED_LOCATION ${binary_dir}/lib/Debug/gtestd.lib
+        PROPERTY IMPORTED_LOCATION ${libgtest_path}
         )
 else()
     set_property(
@@ -35,9 +40,14 @@ endif()
 # メインエントリ追加
 add_library(gtest_main STATIC IMPORTED)
 if(MSVC)
+    if(CMAKE_BUILD_TYPE STREQUAL "Release")
+        set(libgtest_main_path "${binary_dir}/lib/Release/gtest_main.lib")
+    else()
+        set(libgtest_main_path "${binary_dir}/lib/Debug/gtest_maind.lib")
+    endif()
     set_property(
         TARGET gtest_main
-        PROPERTY IMPORTED_LOCATION ${binary_dir}/lib/Debug/gtest_maind.lib
+        PROPERTY IMPORTED_LOCATION ${libgtest_main_path}
         )
 else()
     set_property(
